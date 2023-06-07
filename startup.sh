98411c361094cf57df9fb9b1a04e5f6a929c50ca
matthbish <56136401+matthbish@users.noreply.github.com>
2023-06-07 16:00:35 -0400

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,6 @@
+osascript open-fullscreen.scpt "Slack"
+osascript open-fullscreen.scpt "Microsoft Outlook"
+osascript open-fullscreen.scpt "Microsoft Teams"
+osascript open-fullscreen.scpt "IntelliJ IDEA"
+
+osascript open-zoomed.scpt "Google Chrome"
